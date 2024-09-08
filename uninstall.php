9f17b51863731e00e8dc294b9546195e96f66bc1
elib19 <143918479+elib19@users.noreply.github.com>
2024-09-08 15:34:33 -0300

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,26 +1,26 @@
 <?php
 /**
- * Funções para desinstalação e remoção do plugin
+ * Funções para desinstalação do plugin
  */
 
 // Função de desinstalação do plugin
-function plugin_desativacao() {
+function plugin_desinstalacao() {
     global $wpdb;
 
     // Nome das tabelas
     $tabela_sellers = $wpdb->prefix . 'sellers';
     $tabela_produtos = $wpdb->prefix . 'produtos';
 
-    // SQL para remover as tabelas
-    $sql_remove_sellers = "DROP TABLE IF EXISTS $tabela_sellers;";
-    $sql_remove_produtos = "DROP TABLE IF EXISTS $tabela_produtos;";
+    // Remove as tabelas
+    $wpdb->query("DROP TABLE IF EXISTS $tabela_sellers");
+    $wpdb->query("DROP TABLE IF EXISTS $tabela_produtos");
 
-    // Executa as queries
-    $wpdb->query($sql_remove_sellers);
-    $wpdb->query($sql_remove_produtos);
+    // Remove as opções
+    delete_option('pagarme_api_key');
+    delete_option('pagarme_token');
 }
 
 // Registra a função de desinstalação
-register_uninstall_hook(__FILE__, 'plugin_desativacao');
+register_uninstall_hook(__FILE__, 'plugin_desinstalacao');
 
 ?>
